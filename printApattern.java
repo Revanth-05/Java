b420a800a9ff055b245db0f952e7bcad49134488
Revanth-05 <119917060+Revanth-05@users.noreply.github.com>
2023-04-02 10:42:22 +0530

    Add files via upload

---
+++ printApattern.java
@@ -0,0 +1,12 @@
+ /*  uSing Comments & */
+    //    print a pattern 
+
+public class printApattern {
+    public static void main(String args[]){
+       
+    System.out.println("****");
+    System.out.println("***");
+    System.out.println("**");
+    System.out.println("*");
+    }
+}
