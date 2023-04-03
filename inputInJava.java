77edcedc66f00b4ec036bf4929e2a01de824c52a
Revanth-05 <119917060+Revanth-05@users.noreply.github.com>
2023-04-03 11:43:16 +0530

    Update inputInJava.java
    // input numbers from user and return their sum and multiplication using Java .
    Find Area of Circle using Java?

--- inputInJava.java
+++ inputInJava.java
@@ -12,3 +12,15 @@ public class inputInJava {
 
     }
 }
+
+// // Find Area of a Circle (take input from user)?
+// import java.util.*;
+// public class inputInJava {
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner (System.in);
+//         float r = sc.nextFloat();
+//         float AofCircle = r*r*3.14;
+//         System.out.println(AofCircle); 
+        
+//     }
+// }
