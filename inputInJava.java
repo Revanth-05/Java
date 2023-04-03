fc665403b023562451f0697063a853887a8a7f83
Revanth-05 <119917060+Revanth-05@users.noreply.github.com>
2023-04-03 12:46:37 +0530

    Update inputInJava.java

--- inputInJava.java
+++ inputInJava.java
@@ -1,14 +1,20 @@
-// input numbers from user and return their sum and multiplication of numbers.
 import java.util.*;
 public class inputInJava {
     public static void main(String args[]){
         Scanner sc = new Scanner (System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int sum = a+b;
-        int multiplication = a*b;
-        System.out.println(sum); 
-        System.out.println(multiplication); 
+        
+// // input numbers from user and return their sum and multiplication of numbers.
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sum = a+b;
+//         int multiplication = a*b;
+//         System.out.println(sum); 
+//         System.out.println(multiplication); 
 
+// // Find Area of Circle (take Input from user);
+//         int r = sc.nextInt();
+//         float AofCircle = 3.14 * r * r ;
+//         System.out.println(AofCircle); 
+        
     }
 }
