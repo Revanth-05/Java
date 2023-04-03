c16e08388aa45296a05d789cd2f278c2befdc172
Revanth-05 <119917060+Revanth-05@users.noreply.github.com>
2023-04-03 12:47:58 +0530

    Update inputInJava.java

--- inputInJava.java
+++ inputInJava.java
@@ -4,12 +4,12 @@ public class inputInJava {
         Scanner sc = new Scanner (System.in);
         
 // // input numbers from user and return their sum and multiplication of numbers.
-//         int a = sc.nextInt();
-//         int b = sc.nextInt();
-//         int sum = a+b;
-//         int multiplication = a*b;
-//         System.out.println(sum); 
-//         System.out.println(multiplication); 
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        int multiplication = a*b;
+        System.out.println(sum); 
+        System.out.println(multiplication); 
 
 // // Find Area of Circle (take Input from user);
 //         int r = sc.nextInt();
