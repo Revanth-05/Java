fb839fa824c1f5cec49db52b948619511772a5cb
Revanth-05 <119917060+Revanth-05@users.noreply.github.com>
2023-04-03 12:49:17 +0530

    Add files via upload

---
+++ typeConversionandCastingandPromotion.java
@@ -0,0 +1,46 @@
+// type Conversion and Casting and Promotion in expression
+import java.util.*;
+public class typeConversionandCastingandPromotion {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+
+// // type Conversion
+        // int a = sc.nextInt();
+        // float b = a;
+
+        // long c = sc.nextInt();
+        // System.out.println(b);
+        // System.out.println(c);
+
+        // char ch = 'a';
+        // int d = ch ;
+        // System.out.println(d);
+
+// //  type Casting
+// float f = 99.99f ;
+// int j = (int) f;
+
+// long l = 123;
+// int i = (int)l;
+// System.out.println(j);
+// System.out.println(i);
+
+
+// // type Promotion in expression
+// char a = 'a';
+// char b = 'b';
+
+// short c = 50;
+// System.out.println(b-a);
+// System.out.println(a+c);
+
+// int a = 5;
+// float b = 10.1f;
+// long c = 10;
+// double d = 10.1;
+
+//    double ax = a * b - d - c ; 
+//    System.out.println(ax);
+
+    }
+}
