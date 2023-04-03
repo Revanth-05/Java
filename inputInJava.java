57db7b7fc23a86eb7d96b5b963ceaea1e3f744b1
Revanth-05 <119917060+Revanth-05@users.noreply.github.com>
2023-04-03 10:58:15 +0530

    Add files via upload
     input numbers from user and return the sum of numbers using Java

---
+++ inputInJava.java
@@ -0,0 +1,11 @@
+// input numbers from user and return the sum of numbers.
+import java.util.*;
+public class inputInJava {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        float b = sc.nextFloat();
+        float sum = a+b;
+        System.out.println(sum); 
+    }
+}
