6cb058970d0451650e26088c6187aab494c8143b
Revanth-05 <119917060+Revanth-05@users.noreply.github.com>
2023-04-03 10:26:23 +0530

    Add files via upload

---
+++ dataTypes.java
@@ -0,0 +1,13 @@
+public class dataTypes{
+    public static void main(String args[]){
+        int a = 12;
+        float b = 12.12f;
+        char c = 'a';
+        boolean d = true;
+        double e = 123.123;
+        long f = 123123;
+        short h = 1;
+        byte i = 0;
+        
+    }
+}
