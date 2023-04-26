519d916f09da626a312905b654fcb4bd5c0bcd12
Amula Revanth Kumar <119917060+Revanth-05@users.noreply.github.com>
2023-04-26 15:28:32 +0530

    Add files via upload

---
+++ arraysinputoutput.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class arraysinputoutput {
+
+    // public static void update(int marks[]) {
+    //     // marks[1] = 99;
+    //     // marks[2] += 2;
+    //     // System.out.println("Commerce marks : " + marks[0]);
+    //     // System.out.println("Mathematics marks : " + marks[1]);
+    //     // System.out.println("Economics marks : " + marks[2]);
+    //     for(int i = 0;i < marks.length;i++){
+    //             marks[i] += 1;
+    //         }
+
+    // }
+    public static void main(String[] args) {
+        
+// // Input and Output
+//         int marks[] = new int[5];
+// Scanner sc = new Scanner(System.in);
+
+// System.out.println("Enter the marks");    
+// for(int i = 0;i < marks.length;i++){
+//     marks[i] = sc.nextInt(); 
+// }
+// // // another method totake input 
+// // marks[0] = sc.nextInt();
+// // marks[1] = sc.nextInt();
+// // marks[2] = sc.nextInt();
+// System.out.println("Commerce marks : " + marks[0]);
+// System.out.println("Mathematics marks : " + marks[1]);
+// System.out.println("Economics marks : " + marks[2]);
+
+// marks[1] = 100;
+// System.out.println("Mathematics marks : " + marks[1]); // --> Updating the array
+// System.out.println("Economics marks : " + (marks[2] - 1)); // --> Updation
+// System.out.println("length/Size of array = " + marks.length);
+
+
+// // // Arrays as Functions
+// int marks[] = {99,97,98};
+// update(marks);
+
+// To print updated marks/Values
+// for(int i=0;i<marks.length;i++){
+//     System.out.println(marks[i]);
+// }
+    }
+}
