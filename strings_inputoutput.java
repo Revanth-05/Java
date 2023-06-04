c3f1123246fde3a211f38cec749ee7aafbe53a3c
Amula Revanth Kumar <119917060+Revanth-05@users.noreply.github.com>
2023-06-04 18:22:27 +0530

    Add files via upload

---
+++ strings_inputoutput.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class strings_inputoutput {
+public static void main(String args[]) {
+    // char arr[] = {'a','b','c','d'};
+    // // *** creation
+    // String s = "abcd"; // -->method 1;
+    // String str = new String("Habibi");   // -->method 2;
+
+    // // *** input
+    // Scanner c  = new Scanner(System.in);
+    // // // System.out.println("enter name");
+    // // String name = c.next();  //->.next is used for single word.
+    
+    // System.out.println("enter Full name");
+    // String fullname = c.nextLine(); //->.nextLine is used for the whole .
+
+    // // *** output
+    // System.out.println(str);
+    // // System.out.println(name);
+    // System.out.println(fullname);
+
+    // *** Length
+
+// System.out.println(fullname.length());
+    }
+}
